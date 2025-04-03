b9f9d2c252d19687d5a0bcbf7cde48747b87d31c
Sunny-Dixit <meetus.sunny@gmail.com>
2025-04-03 19:12:16 +0530

    new changes

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9090/',  // Replace with your backend URL
+    baseURL: 'https://a8e9-103-225-187-236.ngrok-free.app/',  // Replace with your backend URL
     headers: {
         'Content-Type': 'application/json',
     },
