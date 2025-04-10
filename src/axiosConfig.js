0476d1760e3bc597d2b89a400a425b58090ae50c
Sunny-Dixit <meetus.sunny@gmail.com>
2025-04-10 21:21:57 +0530

    admin page

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,12 +2,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    //baseURL: 'http://speedy-tyres.ap-south-1.elasticbeanstalk.com/',  // Replace with your backend URL
-    baseURL: 'https://api.speedy-tyres-v1.up7news.in/',
-    //baseURL: 'http://localhost:9090/',
+    //baseURL: 'https://api.speedy-tyres-v1.up7news.in/',
+    baseURL: 'http://localhost:9090/',
 
 
-    //https://api.speedy-tyre.up7news.in/category/all
 
     headers: {
         'Content-Type': 'application/json',
