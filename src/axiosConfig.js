5bc070636d66760a6434967970f6427c5435832d
Sunny-Dixit <meetus.sunny@gmail.com>
2025-04-10 21:43:05 +0530

    fix url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,5 +1,5 @@
 // src/axiosConfig.js
-import axios from 'axios';
+/*import axios from 'axios';
 
 const instance = axios.create({
       baseURL: 'https://api.speedy-tyres-v1.up7news.in/',
@@ -7,6 +7,25 @@ const instance = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    console.log("Using Axios baseURL:", instance.defaults.baseURL);
+
 });
 
+export default instance;*/
+
+// src/axiosConfig.js
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://api.speedy-tyres-v1.up7news.in/',
+  // baseURL: 'http://localhost:9090/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// ✅ Log the base URL AFTER the instance is created
+console.log("Using Axios baseURL:", instance.defaults.baseURL);
+
 export default instance;
+
