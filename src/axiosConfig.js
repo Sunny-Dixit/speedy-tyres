7f8b7da3cdf613f42aa0f1476c478101d3d76459
Sunny-Dixit <meetus.sunny@gmail.com>
2025-04-10 21:33:00 +0530

    main url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,11 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://api.speedy-tyres-v1.up7news.in/',
+      baseURL: 'https://api.speedy-tyres-v1.up7news.in/',
     //baseURL: 'http://localhost:9090/',
-
-
-
     headers: {
         'Content-Type': 'application/json',
     },
