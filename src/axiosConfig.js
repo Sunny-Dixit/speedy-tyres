81d67fb3a5afd9ec2a3f7ea6b0159c7560f727b1
Sunny-Dixit <meetus.sunny@gmail.com>
2025-04-19 00:40:02 +0530

    dermrange url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'https://api.speedy-tyres-v3.up7news.in/',
-  //baseURL: 'https://api.speedy-tyres-mail.dermrange.io/',
+   //baseURL: 'https://api.speedy-tyres-v3.up7news.in/',
+  baseURL: 'https://api.speedy-tyres-mail.dermrange.io/',
    //baseURL: 'https://api.speedy-tyres-v1.up7news.in/',
 
 
